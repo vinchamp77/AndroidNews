2da4447afbba91871b5f8ff1e11070adc4f687bb
vinchamp77 <vinchamp77@gmail.com>
2022-02-05 15:36:47 +1100

    Initial commit

---
+++ app/src/main/java/vtsen/hashnode/dev/androidnews/ui/theme/Theme.kt
@@ -0,0 +1,56 @@
+package vtsen.hashnode.dev.androidnews.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun AndroidNewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    useSystemUIController: Boolean = true,
+    content: @Composable () -> Unit) {
+
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    if (useSystemUIController) {
+        val systemUiController = rememberSystemUiController()
+        systemUiController.setSystemBarsColor(
+            color = colors.primaryVariant
+        )
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}

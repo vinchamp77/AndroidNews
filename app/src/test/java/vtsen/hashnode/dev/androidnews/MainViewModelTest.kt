021812ebb538be5276d3907caa78b0475af55bf7
vinchamp77 <vinchamp77@gmail.com>
2022-04-16 13:11:01 +1000

    Fix unit test error

--- app/src/test/java/vtsen/hashnode/dev/androidnews/MainViewModelTest.kt
+++ app/src/test/java/vtsen/hashnode/dev/androidnews/MainViewModelTest.kt
@@ -24,7 +24,7 @@ class MainViewModelTest {
     @Before
     fun setupViewModel() {
         viewModel = MainViewModel(ApplicationProvider.getApplicationContext())
-        mockViewModel = MainViewModel(ApplicationProvider.getApplicationContext(), mockArticles = true)
+        mockViewModel = MainViewModel(ApplicationProvider.getApplicationContext(), useFakeData = true)
     }
 
     @Test

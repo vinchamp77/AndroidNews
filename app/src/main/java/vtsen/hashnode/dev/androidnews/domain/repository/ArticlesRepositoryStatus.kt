18ca766c09fe75f3a41a6f4adbb5e35e48fc2456
vinchamp77 <vinchamp77@gmail.com>
2022-09-20 09:40:00 +1000

    Change sealed class to sealed interface

--- app/src/main/java/vtsen/hashnode/dev/androidnews/domain/repository/ArticlesRepositoryStatus.kt
+++ app/src/main/java/vtsen/hashnode/dev/androidnews/domain/repository/ArticlesRepositoryStatus.kt
@@ -1,8 +1,8 @@
 package vtsen.hashnode.dev.androidnews.domain.repository
 
-sealed class ArticlesRepositoryStatus {
-    object Invalid: ArticlesRepositoryStatus()
-    object IsLoading: ArticlesRepositoryStatus()
-    data class Success(val withNewArticles: Boolean): ArticlesRepositoryStatus()
-    object Fail: ArticlesRepositoryStatus()
+sealed interface ArticlesRepositoryStatus {
+    object Invalid: ArticlesRepositoryStatus
+    object IsLoading: ArticlesRepositoryStatus
+    data class Success(val withNewArticles: Boolean): ArticlesRepositoryStatus
+    object Fail: ArticlesRepositoryStatus
 }

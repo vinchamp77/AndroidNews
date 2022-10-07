94b006b79ce9fc2f7dc2bf892f71bcd53a7dbd44
vinchamp77 <vinchamp77@gmail.com>
2022-10-07 12:57:48 +1100

    Add default splash screen for < Android 12 (API level 31)

--- app/src/main/java/vtsen/hashnode/dev/androidnews/ui/main/MainActivity.kt
+++ app/src/main/java/vtsen/hashnode/dev/androidnews/ui/main/MainActivity.kt
@@ -3,10 +3,17 @@ package vtsen.hashnode.dev.androidnews.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import vtsen.hashnode.dev.androidnews.data.repository.ArticlesRepositoryImpl
+import vtsen.hashnode.dev.androidnews.ui.main.viewmodel.ArticlesUiState
+import vtsen.hashnode.dev.androidnews.ui.main.viewmodel.ArticlesViewModel
 import vtsen.hashnode.dev.androidnews.ui.main.viewmodel.ArticlesViewModelFactory
 
 class MainActivity : ComponentActivity() {
@@ -15,12 +22,37 @@ class MainActivity : ComponentActivity() {
         ArticlesRepositoryImpl.getInstance(application)
     }
 
+    private val viewModel: ArticlesViewModel by viewModels {
+        ArticlesViewModelFactory(repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setupSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         setContent {
             MainScreen(
-                viewModel(factory = ArticlesViewModelFactory(repository)),
+                viewModel,
                 useSystemUIController = true)
+        }
+    }
+
+    private fun setupSplashScreen() {
+        var keepSplashScreenOn = true
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    keepSplashScreenOn =
+                        uiState is ArticlesUiState.Loading ||
+                        uiState is ArticlesUiState.Invalid
+                }
+            }
+        }
+
+        installSplashScreen().setKeepOnScreenCondition {
+            keepSplashScreenOn
         }
     }
 }
